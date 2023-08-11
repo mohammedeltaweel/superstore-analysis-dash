c430eb171125bbc3fc3957e0d0d64e91da1fb67a
Eltaweel <mohammed.eltaweel@tabaqat.net>
2023-08-11 19:48:13 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1(children='Super Store Analysis', style={'textAlign':'center'}),
+
+])
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
