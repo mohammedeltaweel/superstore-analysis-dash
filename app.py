1a7019779a0a9fee55c727b58aaf9ccc9d690fc4
Eltaweel <mohammed.eltaweel@tabaqat.net>
2023-08-18 18:57:23 +0300

    added data folder

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+import plotly.graph_objects as go
 
-
+df = pd.read_csv("data/raw-data.csv", encoding="windows-1252")
+print(df.head())
 app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1(children='Super Store Analysis', style={'textAlign':'center'}),
 
 ])
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
