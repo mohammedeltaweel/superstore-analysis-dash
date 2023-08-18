0708ca84587ef86f261238393a886e779c8d928b
Eltaweel <mohammedeltaweel40@gmail.com>
2023-08-18 19:28:52 +0300

    upload data to cloud

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
-
-df = pd.read_csv("data/raw-data.csv", encoding="windows-1252")
+df = pd.read_csv("https://github.com/mohammedeltaweel/superstore-analysis-dash/files/12381114/raw-data.csv", encoding="windows-1252")
 print(df.head())
 app = Dash(__name__)
 
