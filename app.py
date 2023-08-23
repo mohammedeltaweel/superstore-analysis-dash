0cfbc002ac30449dea95737f7aad6602b95198ef
Eltaweel <mohammedeltaweel40@gmail.com>
2023-08-23 20:32:01 +0300

    modified requirements.txt

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 df = pd.read_csv("https://github.com/mohammedeltaweel/superstore-analysis-dash/files/12381114/raw-data.csv", encoding="windows-1252")
-print(df.head())
 app = Dash(__name__)
 
 app.layout = html.Div([
